ae3d6f19c2b86245e2de0f73f86de9c624acc39e
AladdinAlswaifi <aladdinosama@hotmail.com>
2022-03-22 10:55:37 +0000

    ready for programming

--- Calculator/static/script.js
+++ Calculator/static/script.js
@@ -1,12 +1,6 @@
 /* When the user clicks on the button,
 toggle between hiding and showing the dropdown content */
-function ContraIndication() {
-    
-    var C_indication = document.getElementById("dropdown-content").value;
-    if (C_indication = "None"){
-        document.getElementById("bmi-status").innerHTML = "None";
-    }
-}
+
   
 
 
@@ -18,22 +12,11 @@ function Calculate(){
     var Age = document.getElementById("A-input").value;
     var Creatinine = document.getElementById("C-input").value;
 
+    var e = document.getElementById("ContraIndication");
+	var Cindication = e.options[e.selectedIndex].text;
 
     var result = parseFloat(weight) /(parseFloat(height)/100)**2;
 
-    if(!isNaN(result)){
-        document.getElementById("bmi-output").innerHTML = result;
-        if(result < 18.5){
-            document.getElementById("bmi-status").innerHTML = "Underweight";
-        }
-        else if(result < 25){
-            document.getElementById("bmi-status").innerHTML = "Healthy";
-        }
-        else if(result < 30){
-            document.getElementById("bmi-status").innerHTML = "Overweight";
-        }
-        else{
-            document.getElementById("bmi-status").innerHTML = "Obesity";
-        }
-    }
+    document.getElementById("bmi-status").innerHTML = Cindication;
+    document.getElementById("bmi-output").innerHTML = result;
 }
